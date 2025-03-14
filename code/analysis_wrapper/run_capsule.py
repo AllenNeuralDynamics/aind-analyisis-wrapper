8bc1d1d9ce16ffb7c2ff0551dfdff8629c6ff856
arjunsridhar12345 <arjun.sridhar@alleninstitute.org>
2025-03-14 02:10:33 +0000

    update reading nwb and linting

--- code/analysis_wrapper/run_capsule.py
+++ code/analysis_wrapper/run_capsule.py
@@ -1,6 +1,14 @@
 import utils
+from pynwb import NWBFile
 
-if __name__ == '__main__':
-    input_json = utils.read_input_json()
-    nwb = utils.read_nwb_from_s3(input_json['s3_location'])
+
+def run_analysis(nwb: NWBFile) -> None:
+    # THIS IS WHERE ANALYSIS SPECIFIC CODE CAN GO?
+    # IMPORT RELEVANT PACKAGES AND USE NWB TO DO WHAT YOU WANT
     pass
+
+
+if __name__ == "__main__":
+    input_json = utils.read_input_json()  # possibly change to pydantic model reading
+    nwb = utils.read_nwb_from_s3(input_json["s3_location"])
+    run_analysis(nwb)
--- code/analysis_wrapper/utils.py
+++ code/analysis_wrapper/utils.py
@@ -1,33 +1,45 @@
+import doctest
 import json
-import s3fs
 import pathlib
+
 import pynwb
+import s3fs
 from hdmf_zarr.nwb import NWBZarrIO
 
-DATA_PATH = pathlib.Path('/data')
 S3_FILESYSTEM = s3fs.S3FileSystem()
 
+
 def read_input_json() -> dict:
-    input_json_path = tuple(DATA_PATH.glob('*.json'))
+    input_json_path = tuple(DATA_PATH.glob("*.json"))
     if not input_json_path:
-        raise FileNotFoundError('No input json model found')
-    
-    with open(input_json_path[0], 'r') as f:
+        raise FileNotFoundError("No input json model found")
+
+    with open(input_json_path[0], "r") as f:
         input_json = json.load(f)
-    
+
     return input_json
 
+
 def read_nwb_from_s3(s3_location: str) -> pynwb.NWBFile:
+    """
+    >>> nwb = read_nwb_from_s3('s3://codeocean-s3datasetsbucket-1u41qdg42ur9/7f1eaf10-01bc-41cb-bc88-6464d0425b51/nwb/behavior_769038_2025-02-10_13-16-09.nwb')
+    >>> type(nwb)
+    <class 'pynwb.file.NWBFile'>
+    """
     if not S3_FILESYSTEM.exists(s3_location):
-        raise FileNotFoundError(f'{s3_location} does not exist')
+        raise FileNotFoundError(f"{s3_location} does not exist")
 
     # TODO: Replace with nwb utils module when ready
     if S3_FILESYSTEM.isdir(s3_location):
-        with NWBZarrIO(s3_location, mode = 'r') as io:
+        with NWBZarrIO(s3_location, mode="r") as io:
             nwb = io.read()
-    else: # have to download and then read
-        with S3_FILESYSTEM.open(s3_location, 'rb') as s3_file:
-            with pynwb.NWBHDF5IO(s3_file, mode = 'r') as io:
+    else:  # have to download and then read
+        with S3_FILESYSTEM.open(s3_location, "rb") as s3_file:
+            with pynwb.NWBHDF5IO(s3_file, mode="r") as io:
                 nwb = io.read()
-    
+
     return nwb
+
+
+if __name__ == "__main__":
+    doctest.testmod()
