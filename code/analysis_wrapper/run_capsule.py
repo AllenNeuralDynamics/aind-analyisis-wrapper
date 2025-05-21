eb599888dd4c7e48d3029b28067df542e21fa8ef
arjunsridhar12345 <arjun.sridhar@alleninstitute.org>
2025-05-21 23:44:05 +0000

    simplify reading input. remove app panel. https://github.com/AllenNeuralDynamics/aind-physio-arch/issues/670

--- code/analysis_wrapper/run_capsule.py
+++ code/analysis_wrapper/run_capsule.py
@@ -1,40 +1,20 @@
 import utils
-from pynwb import NWBFile
+import aind_analysis_results.metadata as metadata
+# ADD USER ANALYSIS IMPORTS
 
+def run_analysis(analysis_job_dict: dict) -> None:
+    processing = metadata.construct_processing_record(analysis_job_dict)
+    if metadata.docdb_record_exists(processing):
+        return
+    
+    ### RUN ANALYSIS CODE HERE
+    # TODO: templates for reading and writing to/from S3
+    # RUN ANALYSIS
 
-def get_input_parser() -> argparse.ArgumentParser:
-    """
-    Creates and returns an argument parser for input arguments.
-
-    Parameters
-    ----------
-    None
-
-    Returns
-    -------
-    argparse.ArgumentParser
-        A configured ArgumentParser object with predefined command-line arguments for:
-        - `--analysis_name`: A string argument for the analysis to be run
-        - `--file_extension`: A string argument for optionally specifying whether or not to look for the given file extension
-
-    """
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--analysis_name", type=str, default="")
-    parser.add_argument("--analysis_version", type=str, default="")
-    parser.add_argument("--analysis_libraries", type=str, default="")
-    parser.add_argument("--analysis_parameters", type=str, default="")
-
-    return parser
-
-
-def run_analysis(nwb: NWBFile) -> None:
-    # THIS IS WHERE ANALYSIS SPECIFIC CODE CAN GO?
-    # IMPORT RELEVANT PACKAGES AND USE NWB TO DO WHAT YOU WANT
-    pass
-
+    s3_bucket = 'PUT PROJECT BUCKET NAME HERE' # REPLACE WITH YOUR BUCKET
+    processing_complete = copy_results_to_s3(processing, s3_bucket)
+    metadata.write_to_docdb(processing_complete)
 
 if __name__ == "__main__":
-    input_json = utils.read_input_json()  # possibly change to pydantic model reading
-    nwb = utils.read_nwb_from_s3(input_json["s3_location"])
-    run_analysis(nwb)
+    analysis_job_dict = utils.read_input_json()
+    run_analysis(analysis_job_dict)
--- code/analysis_wrapper/utils.py
+++ code/analysis_wrapper/utils.py
@@ -1,10 +1,5 @@
-import doctest
 import json
 import pathlib
-
-import pynwb
-import s3fs
-from hdmf_zarr.nwb import NWBZarrIO
 
 DATA_PATH = pathlib.Path("/data")
 S3_FILESYSTEM = s3fs.S3FileSystem()
@@ -41,28 +36,3 @@ def read_input_json() -> dict:
         input_json = json.load(f)
 
     return input_json
-
-
-def read_nwb_from_s3(s3_location: str) -> pynwb.NWBFile:
-    """
-    >>> nwb = read_nwb_from_s3('s3://codeocean-s3datasetsbucket-1u41qdg42ur9/7f1eaf10-01bc-41cb-bc88-6464d0425b51/nwb/behavior_769038_2025-02-10_13-16-09.nwb')
-    >>> type(nwb)
-    <class 'pynwb.file.NWBFile'>
-    """
-    if not S3_FILESYSTEM.exists(s3_location):
-        raise FileNotFoundError(f"{s3_location} does not exist")
-
-    # TODO: Replace with nwb utils module when ready
-    if S3_FILESYSTEM.isdir(s3_location):
-        with NWBZarrIO(s3_location, mode="r") as io:
-            nwb = io.read()
-    else:  # have to download and then read
-        with S3_FILESYSTEM.open(s3_location, "rb") as s3_file:
-            with pynwb.NWBHDF5IO(s3_file, mode="r") as io:
-                nwb = io.read()
-
-    return nwb
-
-
-if __name__ == "__main__":
-    doctest.testmod()
